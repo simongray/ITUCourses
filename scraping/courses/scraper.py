8498e36107202e23bcc952505705132b4c3ed3a5
Anders Fischer-Nielsen <andersfischern@me.com>
2015-04-23 15:30:54 +0200

    Added functionality for parsing every lecture/exercise session.

--- scraping/courses/scraper.py
+++ scraping/courses/scraper.py
@@ -15,35 +15,65 @@ def process_course(link_to_course):
         site.xpath('/html/body/table[2]/tr/td[2]/table[1]/tr[2]/td/form/table//tr[6]//td[2]/text()'),
         site.xpath('/html/body/table[2]/tr/td[2]/table[1]/tr[2]/td/form/table//tr[8]//td[2]/text()'),
         site.xpath('/html/body/table[2]/tr/td[2]/table[1]/tr[2]/td/form/table//tr[9]//td[2]/text()'),
-        site.xpath('/html/body/table[2]/tr/td[2]/table[1]/tr[2]/td/form/table//tr[10]//td[2]/text()'),
-        site.xpath('/html/body/table[2]/tr/td[2]/table[3]/tr[2]/td/table[1]/tr[2]/td[1]/text()'),
-        site.xpath('/html/body/table[2]/tr/td[2]/table[3]/tr[2]/td/table[1]/tr[2]/td[2]/text()'),
-        site.xpath('/html/body/table[2]/tr/td[2]/table[3]/tr[2]/td/table[1]/tr[2]/td[5]/text()'),
-        site.xpath('/html/body/table[2]/tr/td[2]/table[3]/tr[2]/td/table[1]/tr[3]/td[1]/text()'),
-        site.xpath('/html/body/table[2]/tr/td[2]/table[3]/tr[2]/td/table[1]/tr[3]/td[2]/text()'),
-        site.xpath('/html/body/table[2]/tr/td[2]/table[3]/tr[2]/td/table[1]/tr[3]/td[5]/text()'),
+        site.xpath('/html/body/table[2]/tr/td[2]/table[1]/tr[2]/td/form/table//tr[10]//td[2]/text()')
     ]
+
+    time_slots = []
+    for i in range(2, 10):
+        time_slots.append(
+            [
+                get_first(site.xpath(
+                    '/html/body/table[2]/tr/td[2]/table[3]/tr[2]/td/table[1]/tr[' + str(i) + ']/td[1]/text()')),
+                get_first(site.xpath(
+                    '/html/body/table[2]/tr/td[2]/table[3]/tr[2]/td/table[1]/tr[' + str(i) + ']/td[2]/text()')),
+                get_first(site.xpath(
+                    '/html/body/table[2]/tr/td[2]/table[3]/tr[2]/td/table[1]/tr[' + str(i) + ']/td[3]/text()')),
+                get_first(site.xpath(
+                    '/html/body/table[2]/tr/td[2]/table[3]/tr[2]/td/table[1]/tr[' + str(i) + ']/td[5]/text()'))
+            ])
 
     site_data = [get_text_from_table_element(element) for element in site_data]
     first_pass = [text.replace(",", "") for text in site_data]
     second_pass = [text.strip() for text in first_pass]
 
-    return {
-        'name':                     second_pass[0],
-        'semester':                 second_pass[1],
-        'line_of_studies':          second_pass[2],
-        'ects':                     second_pass[3],
-        'language':                 second_pass[4],
-        'minimum_participants':     second_pass[5],
-        'expected_participants':    second_pass[6],
-        'maximum_participants':     second_pass[7],
-        'first_course_day':         second_pass[8],
-        'first_time_slot':          second_pass[9],
-        'first_room':               second_pass[10],
-        'second_course_day':        second_pass[11],
-        'second_time_slot':         second_pass[12],
-        'second_room':              second_pass[13]
+    to_return = {
+        'name': second_pass[0],
+        'semester': second_pass[1],
+        'line_of_studies': second_pass[2],
+        'ects': second_pass[3],
+        'language': second_pass[4],
+        'minimum_participants': second_pass[5],
+        'expected_participants': second_pass[6],
+        'maximum_participants': second_pass[7],
+        'time_slots': convert_to_time_slot_dict(time_slots)
     }
+
+    return to_return
+
+
+def get_first(iterable, default=None):
+    if iterable:
+        for item in iterable:
+            return item
+    return default
+
+
+def convert_to_time_slot_dict(to_convert):
+    converted = []
+
+    for data in to_convert:
+        if get_first(data) is None:
+            pass
+        else:
+            as_dict = {
+                'day': data[0],
+                'time_slot': data[1],
+                'type': data[2],
+                'room': data[3]
+            }
+            converted.append(as_dict)
+
+    return converted
 
 
 def get_text_from_table_element(arr):
