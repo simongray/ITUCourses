1e29df5676aebff125ccffa9efdd87fea1af49ec
Anders Fischer-Nielsen <andersfischern@me.com>
2015-04-29 16:02:16 +0200

    Made stone-age converter.

---
+++ knn-classification/converter.py
@@ -0,0 +1,57 @@
+__author__ = 'Anders'
+
+import loader
+
+# "ects_points": 750,
+# "expected_participants": 30,
+# "job_evaluation": 3.57,
+# "language": "Dansk",
+# "line_of_studies": "Master i it organisation og implementering (ori)",
+# "maximum_participants": 34,
+# "minimum_participants": 0,
+# "name": "Advanced IT Project Management",
+# "overall_evaluation": 3.29,
+# "programme": "MORI",
+# "replies": 0.4117647058823529,
+# "semester": "Efterår 2011",
+# "time_evaluation": 3.0,
+# "time_slots":
+
+
+def convert_line_of_studies(studies):
+    if studies == 'Master i it organisation og implementering (ori)':               return 1
+    if studies == 'cand.it. softwareudvikling og -teknologi (sdt)':                 return 2
+    if studies == 'cand.it. e-business (ebuss)':                                    return 3
+    if studies == 'Master i it interaktionsdesign og multimedier (inm)':            return 4
+    if studies == 'cand.it. digital design og kommunikation (ddk)':                 return 5
+    if studies == 'Master i it it-ledelse og strategi (ils)':                       return 6
+    if studies == 'Bachelor i global virksomhedsinformatik (bgbi)':                 return 7
+    if studies == 'Bachelor i softwareudvikling (bswu)':                            return 8
+    if studies == 'Bachelor i digitale medier og design (bdmd)':                    return 9
+    if studies == 'cand.it. medieteknologi og spil (mtg)':                          return 10
+    if studies == 'Master i it softwarekonstruktion (sok)':                         return 11
+    if studies == 'cand.it. spil (games)':                                          return 12
+    if studies == 'Master i It-ledelse (ilm)':                                      return 13
+    if studies == 'Master i it Interaktionsdesign (ind)':                           return 14
+    if studies == 'Master i it Softwarekonstruktion (sko)':                         return 15
+    if studies == 'cand.it. Digital Innovation & Management (dim)':                 return 16
+    if studies == 'cand.it. design kommunikation og medier (dkm)':                  return 17
+    if studies == 'cand.it. softwareudvikling (swu)':                               return 18
+    if studies == 'cand.it. tværfaglig it-udvikling (tit)':                         return 19
+    if studies == 'Master i it softwareudvikling (profil it-ledelse) (swu_itl)':    return 20
+    if studies == 'cand.it. internet- og softwareteknologi (int)':                  return 21
+    if studies == 'cand.it. it til organisationer (ito)':                           return 22
+    if studies == 'Master i sundheds-it (sit)':                                     return 23
+
+
+def convert_semester(semester):
+    if "Efterår" in semester: return 0
+    if "Forår" in semester: return 1
+
+
+def convert_language(lang):
+    return 0 if lang == 'Dansk' else 1
+
+
+if __name__ == '__main__':
+    print()
