feb459316c54d43ad66ae01ec715db28acb5045a
Anders Fischer-Nielsen <andersfischern@me.com>
2015-04-29 16:02:33 +0200

    Implemented KNN classification.

---
+++ knn-classification/KNN.py
@@ -0,0 +1,15 @@
+
+import loader
+import numpy as np
+
+from sklearn.neighbors import KNeighborsClassifier
+
+data, target = loader.all_evaluation_data()
+training = loader.training_data()
+test = loader.test_data()
+
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(training, target[:-100])
+
+for course in test:
+    print(str(knn.predict(course)))
