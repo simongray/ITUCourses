26d3c8717103a706843ccf03ae09a1972302cbea
Anders Fischer-Nielsen <andersfischern@me.com>
2015-05-03 17:03:00 +0200

    Added method for string hashing for clustering.

--- loading/converter.py
+++ loading/converter.py
@@ -36,5 +36,12 @@ def convert_language(lang):
     return 0 if lang == 'Dansk' else 1
 
 
+def java_string_hashcode(s):
+    h = 0
+    for c in s:
+        h = (31 * h + ord(c)) & 0xFFFFFFFF
+    return ((h + 0x80000000) & 0xFFFFFFFF) - 0x80000000
+
+
 if __name__ == '__main__':
     print()
