bf891dfa923d3c8e22d5eae2e6327c407c21502e
Simon Gray <simongray@gmail.com>
2015-05-12 15:36:36 +0200

    grouped by course name, sorted by semester

--- scraping/dataset/csv_conversion.py
+++ scraping/dataset/csv_conversion.py
@@ -7,4 +7,9 @@ with open('dataset.json', 'r') as dataset_file:
 with open('dataset.csv', 'w') as output_file:
     dict_writer = csv.DictWriter(output_file, dataset[0].keys())
     dict_writer.writeheader()
-    dict_writer.writerows(dataset)
+
+    for row in dataset:
+        # to allow lexical sorting
+        row['semester'] = row['semester'].replace('Forår ', 'a')
+        row['semester'] = row['semester'].replace('Efterår ', 'b')
+        dict_writer.writerow(row)
