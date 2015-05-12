a07736d6686e6114c27db5f49f2e68235bd3cba7
Anders Fischer-Nielsen <andersfischern@me.com>
2015-05-12 17:23:05 +0200

    Played around with clustering on course base data only.

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -2,38 +2,34 @@ __author__ = 'Anders'
 
 from sklearn.cluster import KMeans
 from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
-
 from loading import loader
 
+import numpy as np
 import matplotlib.pyplot as plot
 
 
-data, target = loader.numerical_evaluation_data()
+data, target = loader.numerical_evaluation_data(eval_data=False)
+with_eval, throw_away = loader.numerical_evaluation_data(eval_data=True)
 
-k_means = KMeans(n_clusters=8)
+k_means = KMeans(n_clusters=10)
 k_means.fit(data)
 
 centers = k_means.cluster_centers_
 labels = k_means.labels_
 
-
-clusters = [[], [], [], [], [], [], [], []]
+clusters = [[], [], [], [], [], [], [], [], [], []]
 i = 0
 for label in labels:
     clusters[label].append(data[i])
     i += 1
 
+fig = plot.figure()
+ax = fig.add_subplot(111, projection='3d')
 
-# fig = plot.figure()
-# ax = fig.add_subplot(111, projection='2d')
+colors = ['r', 'g', 'b', 'y', 'c', 'm', 'k', 'w', (.92, 0.74, 0.63), (0.86, 0.182, 0.234)]
 
-colors = ['r', 'g', 'b', 'y', 'c', 'm', 'k', 'w']
-color = 0
-for cluster in clusters:
-    for item in cluster:
-        plot.scatter(item[0], item[2], c=colors[color])
-    color += 1
-
+for entity in with_eval:
+    cluster = k_means.predict(entity[1:])
+    ax.scatter(entity[2], entity[4], entity[0], c=colors[cluster])
 
 plot.show()
